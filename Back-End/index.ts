7d5097a989bb81d0270d19c3ae4b8772d079513f
lam.nguyenquoc1 <lam.nguyenquoc1@vti.com.vn>
2025-02-17 17:33:15 +0700

    thêm hàm xử lý

--- Back-End/index.ts
+++ Back-End/index.ts
@@ -1,5 +1,18 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
+
+const rule = {
+    register: t.Object({
+        name: t.String({ minLength: 1, maxLength: 20 }),
+        email: t.String({ format: 'email'}),
+        password: t.String({ minLength: 8, maxLength: 32 }),
+        address: t.Optional(t.String())
+    })
+}
 
 const app = new Elysia()
+    .get('/', () => 'hello world')
+    .post('/register', async ({body}) => {
+
+    }, { body: rule.register })
 
 app.listen(4001)
