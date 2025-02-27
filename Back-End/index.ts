56bed404ce2a7513ded0c66b60b0d762f4269a36
QuocLam <developers.dion@gmail.com>
2025-02-27 17:09:35 +0700

    sửa đổi cấu trúc hệ thống

--- Back-End/index.ts
+++ Back-End/index.ts
@@ -1,7 +1,11 @@
 import Elysia, { t } from "elysia";
 import { swagger } from '@elysiajs/swagger'
 import controllerAuthen from "./src/controllers/AuthenController";
+import controllerBot from "./src/controllers/BotController";
+import "./src/providers/MongodbProvider";
 
 const app = new Elysia()
     .use(swagger())
+    .use(controllerAuthen)
+    .use(controllerBot)
     .listen(4001)
