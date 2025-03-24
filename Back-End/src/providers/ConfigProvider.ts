ccbe413a71a4b2bba5c8cd9ff29dd49fed26d772
QuocLam <developers.dion@gmail.com>
2025-03-24 22:07:17 +0700

    fix

--- Back-End/src/providers/ConfigProvider.ts
+++ Back-End/src/providers/ConfigProvider.ts
@@ -9,7 +9,8 @@ export default defineProvider(() => {
         PORT: process.env.APP_PORT as string,
         CONNECT : process.env.CONNECT_STRING_MONGODB as string,
         JWT_ALG : process.env.JWT_ALG as string,
-        JWT_SECRET : process.env.JWT_SECRET as string
+        JWT_SECRET : process.env.JWT_SECRET as string,
+        OPENAI_KEY: process.env.OPENAI_KEY as string
     }
 
     return {config}
